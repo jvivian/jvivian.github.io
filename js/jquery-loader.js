795d56848135152081bcca1e0d69eed9afd461c9
John Vivian <jtvivian@gmail.com>
2017-05-04 14:38:32 -0700

    Add page loader

---
+++ js/jquery-loader.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Jvivian on 5/4/17.
+ */
+$(window).load(function() {      //Do the code in the {}s when the window has loaded
+  $("#loader").fadeOut("slow");  //Fade out the #loader div
+});
