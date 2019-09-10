5486ffba67188fc54dd0a49a21658aa5cebf87c0
John Vivian <jtvivian@gmail.com>
2019-09-10 10:07:27 -0700

    New website from scratch

--- js/jquery-loader.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Jvivian on 5/4/17.
- */
-$(window).load(function() {      //Do the code in the {}s when the window has loaded
-  $("#loader").fadeOut("slow");  //Fade out the #loader div
-});
